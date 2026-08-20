5df188625be7a0afd8b5df60bd84461602193245
Lee JaeHyun <myleje@gmail.com>
2026-08-20 19:59:00 +0900

    feat: Player 클래스 추상화

--- Assets/02_Scripts/Character/Player/Player.cs
+++ Assets/02_Scripts/Character/Player/Player.cs
@@ -7,7 +7,7 @@ namespace DungeonMaster.Character.Player
     [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(SpriteRenderer))]
     [RequireComponent(typeof(InputHandler))]
-    public class Player : MonoBehaviour
+    public abstract class Player : MonoBehaviour
     {
         #region 기본 스텟
 
@@ -34,6 +34,30 @@ namespace DungeonMaster.Character.Player
         protected Animator _animator;
         protected SpriteRenderer _spriteRenderer;
         protected InputHandler _inputHandler;
+        #endregion
+        
+        // Facing 처리를 위한 Weapon Arm
+        protected Transform weaponArm;
+
+        #region 유니티 생명주기 메서드
+
+        protected virtual void Awake()
+        {
+            // 초기 체력 설정
+            _currHp = _maxHp;
+            // 컴포넌트 캐싱
+            _rb = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _inputHandler = GetComponent<InputHandler>();
+            
+            // weaponArm 설정
+            weaponArm = transform.Find("Arm");
+            
+            //weaponArm = this.gameObject.GetComponent<Transform>().Find("Arm");
+            // GameObject.Find  => Root 에서 처음부터 재귀적으로 검색
+            // Transform.Find   => 해당 Transform 의 위치에서 부터 재귀적으로 검색
+        }
         #endregion
     }
 }
