ff78042cad3d7b2c6cc25af99d453a101e36c0d0
Lee JaeHyun <myleje@gmail.com>
2026-08-21 20:36:03 +0900

    .

---
+++ Assets/02_Scripts/Character/Data/WarriorSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WarriorSO", menuName = "DungeonMaster/WarriorSO")]
+public class WarriorSO : ScriptableObject
+{
+    [Header("전사 기본 스텟")] 
+    public float maxHp = 150f;
+    public float moveSpeed = 4f;
+    public float attackDamage = 25f;
+    public float attackCooldown = 0.7f;
+    public float defense = 10f;
+}
