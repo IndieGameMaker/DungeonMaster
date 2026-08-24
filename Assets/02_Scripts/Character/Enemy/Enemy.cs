251d34919e5fed46e50cd64e0f339a3095e36dc2
Lee JaeHyun <myleje@gmail.com>
2026-08-24 20:57:24 +0900

    .

--- Assets/02_Scripts/Character/Enemy/Enemy.cs
+++ Assets/02_Scripts/Character/Enemy/Enemy.cs
@@ -1,9 +1,60 @@
+using System;
 using UnityEngine;
+using DungeonMaster.Character.Enemy.FSM;
+using UnityEngine.InputSystem;
 
 namespace DungeonMaster.Character.Enemy
 {
     public class Enemy : MonoBehaviour
     {
-    
+        // 상태 머신 변수 선언
+        protected StateMachine _stateMachine;
+        
+        // 상태 전환 헬퍼 메서드 
+        public void ChangeState(IState newState)
+        {
+            _stateMachine.ChangeState(newState);
+        }
+
+        #region 유니티 생명주기
+
+        protected void Awake()
+        {
+            // 상태머신 초기화
+            _stateMachine = new StateMachine(this);
+            
+            // 초기 상태 설정(IdleState)
+            ChangeState(new IdleState());
+        }
+
+        private void Update()
+        {
+            // 상태 머신 업데이트
+            _stateMachine.Update();
+            
+            TestFSM();
+        }
+        #endregion
+
+        #region 테스트 코드
+        private void TestFSM()
+        {
+            if (Keyboard.current.digit1Key.wasPressedThisFrame)
+            {
+                ChangeState(new IdleState());
+            }
+            
+            if (Keyboard.current.digit2Key.wasPressedThisFrame)
+            {
+                ChangeState(new ChaseState());
+            }
+            
+            if (Keyboard.current.digit3Key.wasPressedThisFrame)
+            {
+                ChangeState(new AttackState());
+            }
+        }
+
+        #endregion
     }
 }
