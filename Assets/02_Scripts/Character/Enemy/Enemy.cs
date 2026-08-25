7471a18030f1f4630258e4f3ff04b604be7bc61a
Lee JaeHyun <myleje@gmail.com>
2026-08-25 19:38:43 +0900

    feat: FSM 리팩토링 2

--- Assets/02_Scripts/Character/Enemy/Enemy.cs
+++ Assets/02_Scripts/Character/Enemy/Enemy.cs
@@ -12,9 +12,13 @@ namespace DungeonMaster.Character.Enemy
         protected StateMachine _stateMachine;
         
         // 상태 전환 헬퍼 메서드 
-        public void ChangeState(IState newState)
+        public void ChangeState<T>() where T : IState
         {
-            _stateMachine.ChangeState(newState);
+            // 딕셔너리에서 저장된 상태(State)를 가져와서 전환
+            if (_states.TryGetValue(typeof(T), out IState state))
+            {
+                _stateMachine.ChangeState(state);
+            }
         }
 
         // 상태를 저장할 딕셔너리 선언
@@ -22,6 +26,15 @@ namespace DungeonMaster.Character.Enemy
         
         // 상태를 초기화 시키는 추상 메서드
         protected abstract void InitStates();
+        
+        // 컴포넌트 캐싱
+        protected Rigidbody2D _rb;
+        protected SpriteRenderer _spriteRenderer;
+        protected Animator _animator;
+        
+        // 애니메이션 해시 추출
+        protected static readonly int hashIsWalk = Animator.StringToHash("IsWalk");
+        protected static readonly int hashHit = Animator.StringToHash("Hit");
         
         #region 유니티 생명주기
 
@@ -31,10 +44,12 @@ namespace DungeonMaster.Character.Enemy
             _stateMachine = new StateMachine(this);
             
             // 초기 상태 설정(IdleState)
-            ChangeState(new IdleState());
+            ChangeState<IdleState>();
             
             // 상태 초기화 호출
             InitStates();
+            // 컴포넌트 초기화
+            InitComponents();
         }
 
         private void Update()
@@ -46,22 +61,33 @@ namespace DungeonMaster.Character.Enemy
         }
         #endregion
 
+        #region 초기화 메서드
+
+        private void InitComponents()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
+        }
+
+        #endregion
+
         #region 테스트 코드
         private void TestFSM()
         {
             if (Keyboard.current.digit1Key.wasPressedThisFrame)
             {
-                ChangeState(new IdleState());
+                ChangeState<IdleState>();
             }
             
             if (Keyboard.current.digit2Key.wasPressedThisFrame)
             {
-                ChangeState(new ChaseState());
+                ChangeState<ChaseState>();
             }
             
             if (Keyboard.current.digit3Key.wasPressedThisFrame)
             {
-                ChangeState(new AttackState());
+                ChangeState<AttackState>();
             }
         }
 
