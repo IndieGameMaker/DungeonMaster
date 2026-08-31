633a12d9e888ec44b20c6566261bf2c1264d60f6
Lee JaeHyun <myleje@gmail.com>
2026-08-31 20:32:22 +0900

    .

---
+++ Assets/02_Scripts/Core/AudioManager.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // 싱글턴(Singleton) 디자인 패턴
+    public static AudioManager Instance { get; private set; }
+
+    #region 유니티 생명주기
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            // 처음 생성된 경우
+            Instance = this;
+            p(gameObject);
+        }
+        else
+        {
+            // 중복해서 생성된 인스턴스 삭제
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
