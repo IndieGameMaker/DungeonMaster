88d27a0d9387220b33ccb9635c825d7ea61a0489
Lee JaeHyun <myleje@gmail.com>
2026-09-16 20:40:34 +0900

    .

--- Assets/02_Scripts/Inventory/Item/ConsumableItemDataSO.cs
+++ Assets/02_Scripts/Inventory/Item/ConsumableItemDataSO.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "DungeonMaster/Item/ConsumableItemData", fileName = "ConsumableItemDataSO")]
 public class ConsumableItemDataSO : ItemData
 {
-    
+    // HP 회복량
+    public float hpRecovery;
+    // MP 회복량
+    public float mpRecovery;
 }
---
+++ Assets/02_Scripts/Inventory/Item/EquipmentItemDataSO.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public enum EquipType
+{
+    Weapon,
+    Armor,
+}
+
+[CreateAssetMenu(menuName = "DungeonMaster/Item/EquipmentItemData", fileName = "EquipmentItemDataSO")]
+public class EquipmentItemDataSO : ItemData
+{
+    // 장비 타입
+    public EquipType equipType;
+    // 공격력
+    public float attackDamage;
+    // 방어력
+    public float defense;
+    // 공격 속도
+    public float attackSpeed;
+}
