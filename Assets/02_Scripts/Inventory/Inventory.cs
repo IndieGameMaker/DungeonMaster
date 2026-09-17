76ad8d4e5eeb07bc254d1a03764bd334eb4ac015
Lee JaeHyun <myleje@gmail.com>
2026-09-17 19:51:23 +0900

    .

--- Assets/02_Scripts/Inventory/Inventory.cs
+++ Assets/02_Scripts/Inventory/Inventory.cs
@@ -6,8 +6,30 @@ namespace DungeonMaster.InventorySystem
 {
     public class Inventory : MonoBehaviour
     {
+        // 슬롯
         [SerializeField] private List<Slot> _slots;
         
+        // 아이템 저장 배열
+        private ItemData[] _items;
+        // 선택된 슬롯의 인덱스 
+        private int _selectedSlotIndex = -1;
+
+        #region 유니티 생명주기
+
+        private void Awake()
+        {
+            // 아이템 초기화
+            _items = new ItemData[_slots.Count];
+            // 슬롯 인덱스 설정
+            for (int i = 0; i < _slots.Count; i++)
+            {
+                _slots[i].slotIndex = i;
+            }
+            
+            // 첫 번째 슬롯을 기본 선택
+            _slots[0].isDefaultSelected = true;
+        }
+
         private void OnEnable()
         {
             Slot.OnSlotSelected += SlotSelected;
@@ -17,6 +39,7 @@ namespace DungeonMaster.InventorySystem
         {
             Slot.OnSlotSelected -= SlotSelected;
         }
+        #endregion
 
 
         #region 슬롯 처리 메서드
@@ -42,7 +65,6 @@ namespace DungeonMaster.InventorySystem
                 slot.selectedMarkImage.enabled = false;
             }
         }
-
         #endregion
     }
 }
