a07ea7cbb7c31749c5f13a85daaebdb5547a9ccc
Lee JaeHyun <myleje@gmail.com>
2026-09-22 20:32:08 +0900

    .

--- Assets/02_Scripts/UI/UIManager.cs
+++ Assets/02_Scripts/UI/UIManager.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void LoadGameScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("GamePlay");
+        // SceneManager.LoadScene(0);
     }
 }
